ca49661d34c820b3b8a16d6e71db2fca1f2d6f2f
Geras1mleo <sviatoslav.harasymchuk@gmail.com>
2022-12-18 00:59:28 +0100

    Queries updated

--- queries_groep70.sql
+++ queries_groep70.sql
@@ -3,6 +3,6 @@ select count(distinct content) from tags where content ilike 'b%';
 select count(username) from likes group by tweetid order by count desc limit 1;
 select count(id) from tweets where length(content) > 140;
 select count(distinct tweetid) from mentions where tweetid in (select tweetid from mentions group by tweetid having count(distinct index) >= 4);
--- todo opgave 6
+select count(tweetid) from retweets r1 inner join tweets t1 on t1.id = r1.tweetid where r1.datetime < t1.datetime + interval '1 hour';
 select count(id) from quotes where van_tweetid = 170454;
 
